30307da8347bb0098bb85016461f61ee00365107
xiaozuo7 <893985729@qq.com>
2024-02-23 13:29:12 +0800

    [fix]tweak

--- boot/config.go
+++ boot/config.go
@@ -5,7 +5,7 @@ import "github.com/spf13/viper"
 func InitConf() {
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
-	viper.AddConfigPath("./config/")
+	viper.AddConfigPath(".")
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(err)
